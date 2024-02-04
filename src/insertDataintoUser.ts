129dbc2ffcf2fa4b69ce35fa29cb471532e3aa9c
Prem <prempravinsapkale@gmail.com>
2024-02-04 20:24:41 +0530

    generated prisma client and added a function to insert user

---
+++ src/insertDataintoUser.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+   email: string,
+   password: string,
+   firstName: string,
+   lastName: string
+) {
+   const user = await prisma.user.create({
+      data: {
+         email,
+         firstName,
+         lastName,
+         password,
+      },
+   });
+
+   console.log(user);
+}
+
+insertUser('admin1@mail.com', '123456', 'harkirat', 'singh');
